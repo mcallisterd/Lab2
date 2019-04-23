72b954e9933837e061870c9a15c6a5b35fe1690f
Daniel McAllister <danny.mcallister@centre.edu>
2019-04-22 21:34:13 -0400

    lots

--- penguins/correlation.js
+++ penguins/correlation.js
@@ -1,6 +1,6 @@
-corr("crafty-penguin-300px.png","bookworm-penguin-300px.png");
+corr("crafty-penguin-300px.png","bookworm-penguin-300px.png",true);
 
-function corr(peng1,peng2){
+function corr(peng1,peng2,first){
   //get their data
   d3.json("classData.json").then(function(data){
     var p1Data;
@@ -27,7 +27,12 @@ function corr(peng1,peng2){
     r*=mixed.reduce(function(total,d){
       return total + ( d[0] - muX ) * ( d[1] - muY );
     },0);
-    drawLine(r,mixed);
+    if(first){
+      circles(r,mixed);
+    }
+    else{
+      drawLine(r,mixed);
+    }
   },
 function(err){
   console.log(err);
@@ -35,12 +40,45 @@ function(err){
 }
 
 function drawLine(r,grades){
-  console.log(r,grades);
+  var height = 500;
+  var width = 1400;
+  var margins={left:30,right:30,top:30,bot:30};
+  var svg = d3.select("svg");
+  var xScale = d3.scaleLinear()
+                 .domain([0,42])
+                 .range([margins.left,width-margins.right]);
+  var yScale = d3.scaleLinear()
+                 .domain([0,1])
+                 .range([height-margins.top,margins.bot]);
+  var first = grades.map(function(d){return d[0];});
+  var second = grades.map(function(d){return d[1];});
+  svg.selectAll("circle#fir")
+     .data(first)
+     .attr("cy",function(d){return yScale(d);})
+     .transition();
+
+  svg.selectAll("circle#sec")
+     .data(second)
+     .attr("cy",function(d){return yScale(d)})
+     .transition();
+
+  var liner = d3.line()
+                .x(function(d){return d.x})
+                .y(function(d){return d.y});
+  //try 2
+  var y1 = d3.mean([d3.mean(first),d3.mean(second)]);
+  var y2 = y1+r*42/100;
+  var points = [{x:xScale(0),y:yScale(y1)},{x:xScale(42),y:yScale(y2)}];
+  console.log(svg.select("path.fit_line"));
+  svg.select("path.fit_line")
+     .attr("d",liner(points))
+     .transition();
+
 }
 
 function graph(){
-  var height = 700;
-  var width = 700;
+  var height = 500;
+  var width = 1400;
 
   var margins={left:30,right:30,top:30,bot:30};
   var svg = d3.select("svg")
@@ -98,4 +136,54 @@ function getPenguins(){
   function(err){
     console.log(err);
 })
+}
+
+function circles(corr_coef,daters){
+  var height = 500;
+  var width = 1400;
+  var margins={left:30,right:30,top:30,bot:30};
+  var svg = d3.select("svg");
+  var xScale = d3.scaleLinear()
+                 .domain([0,42])
+                 .range([margins.left,width-margins.right]);
+  var yScale = d3.scaleLinear()
+                 .domain([0,1])
+                 .range([height-margins.top,margins.bot]);
+  var first = daters.map(function(d){return d[0];});
+  var second = daters.map(function(d){return d[1];});
+  svg.selectAll("circle")
+     .data(first)
+     .enter()
+     .append("circle")
+     .attr("cx",function(d,i){return xScale(i);})
+     .attr("cy",function(d){return yScale(d);})
+     .attr("r",3)
+     .attr("id","fir")
+     .attr("fill","blue");
+
+  svg.selectAll("circle#none")
+     .data(second)
+     .enter()
+     .append("circle")
+     .attr("cx",function(d,i){return xScale(i);})
+     .attr("cy",function(d){return yScale(d);})
+     .attr("r",3)
+     .attr("id","sec")
+     .attr("fill","red");
+
+  var liner = d3.line()
+                .x(function(d){return d.x})
+                .y(function(d){return d.y});
+
+  //try 2
+  var y1 = d3.mean([d3.mean(first),d3.mean(second)]);
+  var y2 = y1+corr_coef*42/100;
+  var points = [{x:xScale(0),y:yScale(y1)},{x:xScale(42),y:yScale(y2)}];
+  console.log(points);
+
+  svg.append("path")
+     .attr("d",liner(points))
+     .attr("stroke","orange")
+     .attr("stroke-width",3)
+     .attr("class","fit_line");
 }
